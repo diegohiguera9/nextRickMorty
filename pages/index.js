6429b4c4d452fac683fb1f49b86c4ca6ec4fcf7a
Diego <drhiguerag@unal.edu.co>
2022-12-04 10:16:43 -0500

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import styles from "../styles/Home.module.scss";
 import Link from "next/link";
 import { useRouter } from "next/router";
